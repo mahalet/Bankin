92fc40fc4eaa8464adc455095005d7baf4f4ef05
owner <owner@owner-PC.attlocal.net>
2017-01-29 13:30:16 -0800

    this is my first banking System

---
+++ Bank/src/SoloLearn.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+
+public class SoloLearn {
+	public static void main(String[] args) {
+	int id = 0;
+	id++;
+    String fName ="dad";
+    String lName ="me";
+    int age = 29;
+    double weight = 145.6;
+    double savingAcc = 5000;
+    double checkingAcc = 10000;
+   // Scanner myvar = new Scanner(System.in);
+    // Assign the value to the variable
+    // from information the user inpute
+//    fName = myvar.nextLine();
+//    lName = myvar.nextLine();
+//    age = myvar.nextInt();
+//    weight = myvar.nextDouble();
+    System.out.println(" Id is "+id);
+    System.out.println(" first name: " +fName);
+    System.out.println(" last name: "+ lName);
+    System.out.println(" age: "+age);
+    System.out.println(" weight: "+weight);
+    System.out.println(" saving is "+savingAcc);
+    System.out.println(" checking is "+checkingAcc);
+	}  
+
+}
